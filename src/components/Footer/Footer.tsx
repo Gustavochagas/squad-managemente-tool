5b909685289959a1621618f5d47c8250ae2b2084
Gustavo Chagas <gustavo.chagas@i-value.com.br>
2020-11-23 16:42:59 -0300

    feat: created Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span className="footer-description">2020 - All rights reserved</span>
+    </div>
+  )
+};
+
+export default Footer;
